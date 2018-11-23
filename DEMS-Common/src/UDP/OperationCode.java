1b09dd35333b313e161e6e3ddea250defea0e5fd
Chris McArthur <prince.chrismc@gmail.com>
2018-11-22 22:42:26 -0500

    adding op codes for comm channels

--- DEMS-Common/src/UDP/OperationCode.java
+++ DEMS-Common/src/UDP/OperationCode.java
@@ -29,6 +29,12 @@ package UDP;
  */
 public enum OperationCode {
     INVALID(-1),
+    
+    // For FE-SEQ
+    SERIALIZE(1011),
+    ACK_SERIALIZE(3011),
+    
+    // For FE-PI Communication
     UPDATE_RECORD_INDEX(1001),
     ACK_UPDATE_RECORD_INDEX(3001),
     GET_RECORD_COUNT(1002),
@@ -36,7 +42,17 @@ public enum OperationCode {
     DOES_RECORD_EXIST(1003),
     ACK_DOES_RECORD_EXIST(3003),
     TRANSFER_RECORD(1004),
-    ACK_TRANSFER_RECORD(3004);
+    ACK_TRANSFER_RECORD(3004),
+    
+    // For PI-FE
+    OPERATION_RETVAL(1005),
+    ACK_OPERATION_RETVAL(3005),
+
+    // For FE-RM
+    NO_RESP_NOTIFICATION(1201),
+    ACK_NO_RESP_NOTIFICATION(3201),
+    FAULY_RESP_NOTIFICATION(1202),
+    ACK_FAULY_RESP_NOTIFICATION(3202);
 
     private OperationCode(int val) {
         m_Value = val;
@@ -50,10 +66,18 @@ public enum OperationCode {
     public OperationCode toAck(){
         if( m_Value < 3000 ){
             switch( this ){
+                case SERIALIZE: return ACK_SERIALIZE;
+                
                 case UPDATE_RECORD_INDEX: return ACK_UPDATE_RECORD_INDEX;
                 case GET_RECORD_COUNT: return ACK_GET_RECORD_COUNT;
                 case DOES_RECORD_EXIST: return ACK_DOES_RECORD_EXIST;
                 case TRANSFER_RECORD: return ACK_TRANSFER_RECORD;
+                
+                case OPERATION_RETVAL: return ACK_OPERATION_RETVAL;
+                
+                case NO_RESP_NOTIFICATION: return ACK_NO_RESP_NOTIFICATION;
+                case FAULY_RESP_NOTIFICATION: return ACK_FAULY_RESP_NOTIFICATION;
+                
                 default: return INVALID;
             }
         }
