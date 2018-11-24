92d87425f99ac2df4882f6eeb0c57a978c5607ef
winterhart <m.user.work@gmail.com>
2018-11-24 17:21:26 -0500

    #7 basic class to create X number of runnable managers

---
+++ Replica-Manager/Manager/Manager.java
@@ -0,0 +1,12 @@
+package Manager;
+
+public class Manager implements Runnable {
+	
+	public Manager() {}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub, start UDP Server
+		
+	}
+}
