7e6985c26a4de80cc75d8558f1f5f1408a8cc245
winterhart <m.user.work@gmail.com>
2018-11-24 18:11:05 -0500

    #7 addition of basic impl. based on UDP common class

--- Replica-Manager/Manager/Manager.java
+++ Replica-Manager/Manager/Manager.java
@@ -1,12 +1,47 @@
 package Manager;
 
-public class Manager implements Runnable {
-	
-	public Manager() {}
+import Models.AddressBook;
+import UDP.Message;
+import UDP.RequestListener;
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub, start UDP Server
+public class Manager implements RequestListener.Processor {
+	
+	//The replica linked to the manager
+	private String replicaName;
+	final private RequestListener managerListener;
+	private Thread managerListenerThread;
+	public Manager(String assignedReplica, AddressBook listenForAddress) {
+		
+		this.replicaName = assignedReplica;
+		this.managerListener = new RequestListener(this, listenForAddress);
 		
 	}
+
+	@Override
+	public String handleRequestMessage(Message msg) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void Launch() {
+		managerListenerThread = new Thread(managerListener);
+		managerListenerThread.start();
+		//managerListenerThread.wait();
+	}
+	
+	
+	
+	public void Shutdown() {
+		managerListener.Stop();
+		try {
+			managerListenerThread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Can't stop the thread " + this.managerListenerThread.getName());
+		}
+	}
+	
+	
+
 }
