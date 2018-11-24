5b80b1b8943a227b07da893ff835f1ddaf9fc8ef
winterhart <m.user.work@gmail.com>
2018-11-24 17:21:26 -0500

    #7 basic class to create X number of runnable managers

---
+++ Replica-Manager/App/ManagersMap.java
@@ -0,0 +1,23 @@
+package App;
+
+import java.util.HashMap;
+import Manager.Manager;
+
+public class ManagersMap extends HashMap<String, Manager> {
+	private static ManagersMap inst = null;
+	private ManagersMap() {};
+	
+	public static void addManager(Manager manager) {
+		//TODO: Add Managers
+	}
+	
+	public static ManagersMap getManagers() {
+		//TODO: Add implementation
+		return null;
+	}
+	
+	
+	
+	
+
+}
