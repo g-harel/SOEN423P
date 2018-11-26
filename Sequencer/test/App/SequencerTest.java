490dd6ccba055079d415f5c2cecfcc11f70fddb7
Gabriel <gabrielj.harel@gmail.com>
2018-11-26 16:39:48 -0500

    fix sequencer tests for netbeans

--- Sequencer/test/App/SequencerTest.java
+++ Sequencer/test/App/SequencerTest.java
@@ -32,6 +32,9 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import org.junit.After;
 import org.junit.Test;
+
+import App.Sequencer;
+
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
